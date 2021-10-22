ad5ca6ff977d8a5b43eabc31e85df4ae06546786
Alexandre Rebello <alexcandrecrebello@gmail.com>
2021-10-22 09:23:15 -0300

    Configurated communication channel between a client and a server. Backedn Finished.

--- NODE_HEAT/prisma/migrations/20211020154837_create_messages/migration.sql
+++ NODE_HEAT/prisma/migrations/20211020154837_create_messages/migration.sql
@@ -3,6 +3,6 @@ CREATE TABLE "messages" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "text" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
+    "user id" TEXT NOT NULL,
     CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
